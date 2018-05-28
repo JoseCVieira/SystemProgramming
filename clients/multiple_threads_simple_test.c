a84206b877bf63b1b2004fc1b93e212166ae1193
pcarmo <pedro.esperanca.carmo@gmail.com>
2018-05-28 15:00:43 +0100

    mult threads

--- clients/multiple_threads_simple_test.c
+++ clients/multiple_threads_simple_test.c
@@ -1,4 +1,3 @@
-
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/socket.h>
@@ -16,79 +15,80 @@
 #include <pthread.h>
 #include <ifaddrs.h>
 #include <errno.h>
-#include "../library/clipboard.h"
-#define NTHREADS 5
+
+#include "src/clipboard.h"
+
+#define NTHREADS 30
 #define NREGIONS 10
 #define MSGSIZE 100
+
 void print_with_time(char * user_msg);
 void test_string(char * user_msg,int id, int i);
 void * thread_handler(void * args);
 
-pthread_mutex_t id_mutex;
-//cond. variable
 pthread_cond_t data_cond = PTHREAD_COND_INITIALIZER;
-int region = NREGIONS;
+pthread_mutex_t mutex    = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_nr = PTHREAD_MUTEX_INITIALIZER;
 
-int main(int argv, char *argc[]){
+int nr = 0;
 
-    int  i,retval;
-    if(argv  == 2)
-	   region = atoi(argc[1]);
+int main(){
+    int i, retval;
     pthread_t thread_id;
-    for(i = 0; i < NTHREADS; i++){
-      printf("->%d\n", i);
-      pthread_mutex_lock(&id_mutex);
-      if(pthread_create(&thread_id, NULL, thread_handler, &i) != 0)
-          printf("erro");
 
-      pthread_mutex_lock(&id_mutex);
-      pthread_mutex_unlock(&id_mutex);
+    for(i = 0; i < NTHREADS; i++){
+        pthread_mutex_lock(&mutex);
+
+        if(pthread_create(&thread_id, NULL, thread_handler, &i) != 0)
+            printf("erro");
+
+        pthread_mutex_lock(&mutex);
+        pthread_mutex_unlock(&mutex);
     }
-    pthread_join(thread_id,(void *) &retval);
+
+    while(nr != NTHREADS);
+
     return 0;
 }
 
-void *thread_handler(void * args){
-  int id = *((int*)args);
-  pthread_mutex_unlock(&id_mutex);
-    int  i;
-  char *buf;
-  if ((buf = (char*) malloc(  MSGSIZE * sizeof(char) )) == NULL){
-      perror("[error] malloc");
-      exit(-1);
-  }
+void * thread_handler(void * args){
+    int i, id = *((int*)args);
+    pthread_mutex_unlock(&mutex);
 
-  printf("\n");
-  /* Connect to clipboard (server) */
- sprintf(buf, "./socket_%d", getpid());
-  int sock_fd = clipboard_connect(buf);
+    char *buf;
 
-  if(sock_fd == -1){
-      perror("[error] connect");
-      exit(-1);
-  }
+    if ((buf = (char*) malloc(  MSGSIZE * sizeof(char) )) == NULL){
+        perror("[error] malloc");
+        exit(-1);
+    }
 
-  if(region == NREGIONS){
-    for(i = 0; i < NREGIONS; i++){
-      test_string(buf,id, i);
+    sprintf(buf, "./socket_%d", getpid());
+    int sock_fd = clipboard_connect(buf);
 
-      clipboard_copy(sock_fd, i,buf,MSGSIZE );
+    if(sock_fd == -1){
+        perror("[error] connect");
+        exit(-1);
     }
 
     for(i = 0; i < NREGIONS; i++){
-      clipboard_paste(sock_fd, i,buf,MSGSIZE );
-      printf("%s\n", buf);
+        test_string(buf,id, i);
+        clipboard_copy(sock_fd, i, buf, MSGSIZE);
     }
-  }else{
-  	test_string(buf, id, region);
-    printf("sent->%s\n",buf);
-  	clipboard_copy(sock_fd, region, buf, MSGSIZE);
-    clipboard_paste(sock_fd, region,buf,MSGSIZE );
-    printf("%s\n", buf);
-  }
 
-  free(buf);
-  return 0;
+    for(i = 0; i < NREGIONS; i++){
+        clipboard_paste(sock_fd, i, buf, MSGSIZE);
+        printf("%s\n", buf);
+    }
+
+    clipboard_close(sock_fd);
+
+    free(buf);
+
+    pthread_mutex_lock(&mutex);
+    nr++;
+    pthread_mutex_unlock(&mutex);
+
+    pthread_exit(NULL);
 }
 
 void print_with_time(char * user_msg){
@@ -97,13 +97,16 @@ void print_with_time(char * user_msg){
     tm_struct = localtime(&time_v);
     printf("<%02d:%02d:%02d> %s\n", tm_struct->tm_hour, tm_struct->tm_min, tm_struct->tm_sec, user_msg);
 }
+
 void test_string(char * user_msg, int id, int i){
     struct tm *tm_struct;
+
     if (realloc(user_msg, 100*sizeof(char) ) == NULL){
         perror("[error] malloc");
         exit(-1);
     }
+
     time_t time_v = time(NULL);
     tm_struct = localtime(&time_v);
-    sprintf(user_msg, "<%02d:%02d:%02d:> thread with ID: %d wrote on region %d\n",tm_struct->tm_hour, tm_struct->tm_min, tm_struct->tm_sec, id, i);
+    sprintf(user_msg, "<%02d:%02d:%02d> thread with ID: %d wrote on region %d\n",tm_struct->tm_hour, tm_struct->tm_min, tm_struct->tm_sec, id, i);
 }
